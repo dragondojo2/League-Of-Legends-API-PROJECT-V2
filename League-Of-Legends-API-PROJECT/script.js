6bba30c1b91a36c74994bece84654a265f2e5f7b
João Gabriel <68438997+dragondojo2@users.noreply.github.com>
2020-08-24 19:38:08 -0300

    UI improvement, All working witch back-end, Setted the placeholders

--- League-Of-Legends-API-PROJECT/script.js
+++ League-Of-Legends-API-PROJECT/script.js
@@ -1,6 +1,6 @@
 var app = angular.module('mainApp', []);
 
-app.controller('angularController', function ($scope, $http) {
+app.controller('angularController', function ($scope, $http,$sce) {
 
     $scope.displayOn = false
     $scope.displayOff = true
@@ -9,26 +9,13 @@ app.controller('angularController', function ($scope, $http) {
 
     function padLeft(id, str){
         $scope.total = Array(4-String(id).length+1).join(str||'0')+id;
-        console.log($scope.total);
     }
-
-    app.directive('onErrorSrc', function() {
-        return {
-            link: function(scope, element, attrs) {
-              element.bind('error', function() {
-                if (attrs.src != attrs.onErrorSrc) {
-                  attrs.$set('src', attrs.onErrorSrc);
-                }
-              });
-            }
-        }
-    });
 
     $scope.flipForFront = function () {
-        document.getElementById('test1').style='transform: rotateY(0deg);'
+        document.getElementById('rotate').style='transform: rotateY(0deg);'
     }
     $scope.flipForBack = function() {
-        document.getElementById('test1').style='transform: rotateY(-180deg);'
+        document.getElementById('rotate').style='transform: rotateY(-180deg);'
     }
     $scope.viewOn = function () {
         var remove = document.getElementsByClassName('remove')
@@ -52,30 +39,76 @@ app.controller('angularController', function ($scope, $http) {
             method: 'GET',
             url: 'http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/' + champion + '.json'
         }).then(function sucessCallBack(response) {
-            console.log(response);
+            console.log(response)
             $scope.id = response.data.data[champion]["id"]
             let skinId = response.data.data[champion]["skins"][0]['id']
             let newIdValue = skinId/1000
-            padLeft(newIdValue)
-            $scope.lore = response.data.data[champion]["lore"]
+
             const skillP = response.data.data[champion]["passive"]["image"]["full"]
             const skillQ = response.data.data[champion]["spells"][0]["image"]["full"]
             const skillW = response.data.data[champion]["spells"][1]["image"]["full"]
             const skillE = response.data.data[champion]["spells"][2]["image"]["full"]
             const skillR = response.data.data[champion]["spells"][3]["image"]["full"]
-            $scope.skillPDesc = response.data.data[champion]["passive"]["description"]
-            $scope.skillQDesc = response.data.data[champion]["spells"][0]["description"]
-            $scope.skillWDesc = response.data.data[champion]["spells"][1]["description"]
-            $scope.skillEDesc = response.data.data[champion]["spells"][2]["description"]
-            $scope.skillRDesc = response.data.data[champion]["spells"][3]["description"]
-            let skillWDescc = $scope.skillWDesc
-            $scope.stripedHtml = skillWDescc.replace(/<[^>]+>/g, '');
-            console.log($scope.stripedHtml);
-            $scope.videoP = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0142/ability_0142_W1.webm"
-            $scope.videoQ = 'https://d28xe8vt774jo5.cloudfront.net/champion-abilities/'+ $scope.total +'/ability_'+ $scope.total +'_Q1.webm'
-            $scope.videoW = 'https://d28xe8vt774jo5.cloudfront.net/champion-abilities/'+ $scope.total +'/ability_'+ $scope.total +'_W1.webm'
-            $scope.videoE = 'https://d28xe8vt774jo5.cloudfront.net/champion-abilities/'+ $scope.total +'/ability_'+ $scope.total +'_E1.webm'
-            $scope.videoR = 'https://d28xe8vt774jo5.cloudfront.net/champion-abilities/'+ $scope.total +'/ability_'+ $scope.total +'_R1.webm'
+            $scope.skillP = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/passive/" + skillP
+            $scope.skillQ = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillQ
+            $scope.skillW = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillW
+            $scope.skillE = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillE
+            $scope.skillR = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillR
+
+            $scope.skillPName = response.data.data[champion]["passive"]["name"]
+            $scope.skillQName = response.data.data[champion]["spells"][0]["name"]
+            $scope.skillWName = response.data.data[champion]["spells"][1]["name"]
+            $scope.skillEName = response.data.data[champion]["spells"][2]["name"]
+            $scope.skillRName = response.data.data[champion]["spells"][3]["name"]
+            
+            $scope.video = function (skill) {
+                console.log(skill)
+                 
+                let [skillPDesc,skillQDesc,skillWDesc,skillEDesc,skillRDesc] = [response.data.data[champion]["passive"]["description"],response.data.data[champion]["spells"][0]["description"],response.data.data[champion]["spells"][1]["description"],response.data.data[champion]["spells"][2]["description"],response.data.data[champion]["spells"][3]["description"]]                
+                
+                if(skill === "P"){
+                    let stripedHtmlP = skillPDesc.replace(/<[^>]+>/g, '');
+                    document.getElementById('skillContent').innerHTML = `${stripedHtmlP}`
+                    $scope.link = document.getElementById('skillImage').innerHTML = `${$scope.skillP}`
+                    document.getElementById('skillTitle').innerHTML = `${$scope.skillPName}`
+                }else if (skill === "Q") {
+                    let stripedHtmlQ = skillQDesc.replace(/<[^>]+>/g, '');
+                    document.getElementById('skillContent').innerHTML = `${stripedHtmlQ}`
+                    $scope.link = document.getElementById('skillImage').innerHTML = `${$scope.skillQ}`
+                    document.getElementById('skillTitle').innerHTML = `${$scope.skillQName}`
+                }else if (skill === "W") {
+                    let stripedHtmlW = skillWDesc.replace(/<[^>]+>/g, '');
+                    document.getElementById('skillContent').innerHTML = `${stripedHtmlW}`
+                    $scope.link = document.getElementById('skillImage').innerHTML = `${$scope.skillW}`
+                    document.getElementById('skillTitle').innerHTML = `${$scope.skillWName}`
+                }else if (skill === "E") {
+                    let stripedHtmlE = skillEDesc.replace(/<[^>]+>/g, '');
+                    document.getElementById('skillContent').innerHTML = `${stripedHtmlE}`
+                    $scope.link = document.getElementById('skillImage').innerHTML = `${$scope.skillE}`
+                    document.getElementById('skillTitle').innerHTML = `${$scope.skillEName}`
+                }else if (skill === "R") {
+                    let stripedHtmlR = skillRDesc.replace(/<[^>]+>/g, '');
+                    document.getElementById('skillContent').innerHTML = `${stripedHtmlR}`
+                    $scope.link = document.getElementById('skillImage').innerHTML = `${$scope.skillR}`
+                    document.getElementById('skillTitle').innerHTML = `${$scope.skillRName}`
+                }
+               
+               
+               
+                
+                
+
+                padLeft(newIdValue)
+                $scope.preview = 'https://d28xe8vt774jo5.cloudfront.net/champion-abilities/'+ $scope.total +'/ability_'+ $scope.total + '_' + skill + '1.webm' 
+        
+                $scope.trustSrc = function(src) {
+                    return $sce.trustAsResourceUrl(src);
+                };
+                
+            }
+           
+            $scope.lore = response.data.data[champion]["lore"]
+
             $scope.attackDamage = response.data.data[champion]["stats"]["attackdamage"]
             $scope.range = response.data.data[champion]["stats"]["attackrange"]
             $scope.moveS  = response.data.data[champion]["stats"]["movespeed"]
@@ -85,14 +118,11 @@ app.controller('angularController', function ($scope, $http) {
             $scope.lifeRegen = response.data.data[champion]["stats"]["hpregen"]
             $scope.manaRegen = response.data.data[champion]["stats"]["mpregen"]
             $scope.magicResistence = response.data.data[champion]["stats"]["spellblock"]
+            
+            const fullSplash = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champion + "_0.jpg"
             $scope.splash = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champion + "_0.jpg"
             $scope.icon = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/" + champion + ".png"
-            $scope.skillP = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/passive/" + skillP
-            $scope.skillQ = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillQ
-            $scope.skillW = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillW
-            $scope.skillE = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillE
-            $scope.skillR = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + skillR
-
+           
             $scope.skinNext = function () {
                 count++
                 let caminhoLength = response.data.data[champion]["skins"]
@@ -100,49 +130,26 @@ app.controller('angularController', function ($scope, $http) {
                     count--
                 }
                 let caminho = response.data.data[champion]["skins"][count]
-                console.log("count = " + count);
                 $scope.splash = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champion + "_" + caminho.num + ".jpg"
             }
 
             $scope.skinPrev = function () {
                 count --
                 if(count <= 0){count = 0}
-                let arrayNormalLength = response.data.data[champion]["skins"]
                 let arrayNormal = response.data.data[champion]["skins"][count]
-                console.log("count = " + count);
                 $scope.splash = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champion + "_" + arrayNormal.num + ".jpg"
             }
+            
 
-            console.log('total total' + $scope.total);
+            let changeFullSplash = () =>{
+                document.getElementById('exampleModal').style='background-image: url("' + fullSplash + '");'
+            }
+
+            changeFullSplash()
 
         }).then(function errorCallBack(response) {
         
         })
     }
     
-    // $scope.name = "Annie"
-    // $http({
-    //     method: 'GET',
-    //     url: 'http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json'
-    // }).then(function successCallback(response) {
-    //     $scope.loading = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + $scope.name + "_0.jpg"
-    //     
-    // }, function errorCallback(response) {
-
-    // });
-    // $http({
-    //     method: 'GET',
-    //     url: 'http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/'+ $scope.name + '.json'
-    // }).then(function successCallback(response) {
-    //     console.log(response);
-    //     $scope.skillP = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/passive/" + $scope.name + "_Passive.png"
-    //     console.log($scope.skillP);
-    //     $scope.skillQ = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + $scope.name + "Q.png"
-    //     $scope.skillW = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + $scope.name + "W.png"
-    //     $scope.skillE = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + $scope.name + "E.png"
-    //     $scope.skillR = "http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/" + $scope.name + "R.png"
-    //     console.log($scope.teste);
-    // }, function errorCallback(response) {
-
-    // });
 });
